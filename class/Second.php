16398574439a0eebb192ec90bf8b63c0f64204b6
Shantonu Suman <sumancsediu@gmail.com>
2020-05-24 00:06:00 +0600

    Access property from class through object

--- class/Second.php
+++ class/Second.php
@@ -3,20 +3,34 @@
 class Second{
     var $name = "Shantonu suman";
     var $age = "30" ;
-
+     
+    
+    function showName(){
+        echo "I am from method name \n";
+        echo $this->name."\n";
+    }
+    
     function showAge(){
-        echo "I am from method age";
+        echo "I am from method age \n";
+        echo $this->age ."\n";
 
     }
 
-    function showName(){
-        echo "I am from method name \n";
-        echo $this->name;
+
+    function showEverything(){
+        echo "here showEverythingMethod is calling \n";
+        $this->showName() . "\n";
+        $this->showAge() . "\n";
     }
 }
 $suman = new Second;
 echo $suman->name = "Shantonu suman \n";
 echo $suman->age . "\n";
 echo $suman->showName();
+
+
+$third = new Second;
+
+echo $third->showEverything();
 
 
